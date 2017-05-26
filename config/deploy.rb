5f3c5724c93ee3b0cbfe24af58e7f52690b5f67b
daisuke13 <people.in.the.box.13@gmail.com>
2017-05-26 12:59:05 +0900

    fix url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.8.1"
 
 set :application, 'airbnb'
-set :repo_url, 'git@github.com:airbnb.git'
+set :repo_url, 'git@github.com:daisuke13/airbnb.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
