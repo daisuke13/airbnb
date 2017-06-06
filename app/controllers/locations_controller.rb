e882398aa1eddd74c46540445d58c92cd814d3b7
daisuke13 <people.in.the.box.13@gmail.com>
2017-06-07 06:04:07 +0900

    hosting

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -4,7 +4,6 @@ class LocationsController < ApplicationController
   end
 
   def create
-    binding.pry
     @location = Location.new(location_params)
     if @location.save
       redirect_to new_room_price_path(@location)
